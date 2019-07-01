176df5a818bdafd594ca8a246afb9d26a77a1c48
KrzysztofKarch <krzysztof.karch@wp.pl>
2019-07-01 16:15:48 +0200

    added new file

---
+++ chapter 1/exercise 3.py
@@ -0,0 +1,8 @@
+# "Python Programming for the Absolute Beginner", Michael Dawson
+#
+# chapter 1, exercise 3
+
+print("\"It's only cheating if you get caught. \"")
+print("Al Bundy")
+
+input("\nPress Enter to exit. ")

eb153ab598267d561db26e6bc1b79f66763bea1d
KrzysztofKarch <krzysztof.karch@wp.pl>
2019-07-02 09:51:52 +0200

    Changes to be committed: 	new file:   chapter 2/exercise 1.py

---
+++ chapter 2/exercise 1.py
@@ -0,0 +1,29 @@
+# "Python Programming for the Absolute Beginner", Michael Dawson
+#
+# chapter 2, exercise 1
+
+print("""***********************************
+*    List of correct variables    *
+***********************************""")
+print("score")
+print("max_score")
+print("maxScore")
+print("score2")
+print("score_2")
+
+print("\nRules: the first char must be small letter, \n\
+      digits are allowed, \n\
+      underscore is allowed but not first, \n\
+      special signs are not allowed. \n")
+
+print("""***********************************
+*   List of incorrect variables   *
+***********************************""")
+print("Score")
+print("MAX_SCORE")
+print("2score")
+print("score_@")
+print("_score")
+print("__score")
+print("score64328997")
+input()

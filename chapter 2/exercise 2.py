6df7a5180722ff0b0fb013d58ba152b32c247931
KrzysztofKarch <krzysztof.karch@wp.pl>
2019-07-04 16:57:16 +0200

    new file:   chapter 2/exercise 2.py

---
+++ chapter 2/exercise 2.py
@@ -0,0 +1,11 @@
+# "Python Programming for the Absolute Beginner", Michael Dawson
+#
+# chapter 2, exercise 2
+
+print("Program to making new special meals. ")
+fav_meal = input("What's your favourite meal? ")
+fav_meal2 = input("What's your second favourite meal? ")
+
+print("\n\nGreat! Let's try cook", fav_meal + fav_meal2)
+print("It should be Twice as good!")
+input("\n\nPress Enter to exit. ")

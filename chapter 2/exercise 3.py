ddc960fc8cf09d57d8d18741cc368bb58c8bf743
KrzysztofKarch <krzysztof.karch@wp.pl>
2019-07-05 19:32:45 +0200

    new file:   chapter 2/exercise 3.py

---
+++ chapter 2/exercise 3.py
@@ -0,0 +1,11 @@
+# "Python Programming for the Absolute Beginner", Michael Dawson
+#
+# chapter 2, exercise 3
+
+print("Program to calculating tip in restaurant. ")
+bill = float(input("Enter your bill: "))
+
+print("\n15% tip:", bill * 0.15)
+print("20% tip:", bill * 0.2)
+
+input("\n\nPress Enter to exit. ")

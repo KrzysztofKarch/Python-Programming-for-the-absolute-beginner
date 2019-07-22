378b4e9ec39bc36264e23165104c92f428ad1d89
KrzysztofKarch <krzysztof.karch@wp.pl>
2019-07-22 10:59:09 +0200

    new file:   chapter 5/exercise 4.py

---
+++ chapter 5/exercise 4.py
@@ -0,0 +1,90 @@
+# "Python Programming for the Absolute Beginner", Michael Dawson
+#
+# chapter 5, exercise 4
+
+family={'Michael Douglas':'Kirk Douglas',
+        'Kirk Douglas':'Herszel Danielovitch',
+          'Maciej Stuhr':'Jerzy Stuhr',
+        'Jerzy Stuhr':'Leopold Stuhr',
+          'Angelina Jolie':'Jon Voight',
+        'Jon Voight':'Elmer Voight',
+          'Charlie Sheen':'Martin Sheen',
+        'Martin Sheen':'Francisco Estevez Martinez',
+          'Kate Hudson':'Kurt Russel',
+        'Kurt Russel':'Bing Russel'}
+
+choice=None
+while choice!='7':
+    print("*************************************")
+    print("*  Kto jest Twoim tatą i dziadkiem  *")
+    print("*************************************\n")
+
+    print("1 - wyświetl cały słownik")
+    print("2 - znajdź ojca")
+    print("3 - znajdź dziadka")
+    print("4 - dodaj parę syn-ojciec")
+    print("5 - zmień ojca")
+    print("6 - usuń parę")
+    print("7 - zakończ\n")
+
+    choice=input("Wybierasz: ")
+
+    if choice=='1':
+        print()
+        print("%-20s" % "Syn:", "%-20s" % "Ojciec:")
+        for son in family:     
+            print("%-20s" % son, "%-20s" % family[son])
+        input("\nWciśnij ENTER aby kontynuować...\n")
+
+    elif choice=='2':
+        find=input("Wpisz Imię Nazwisko: ")
+        if find in family:
+            print("\nOjciec: ")
+            print(family[find])
+        else:
+            print("\nBrak informacji o takiej osobie. ")
+        input("\nWciśnij ENTER aby kontynuować...\n")
+
+    elif choice=='3':
+        father=input("Wpisz Imię Nazwisko: ")
+        if father in family:
+            grandfather=family[father]
+            if grandfather in family:
+                print("\nDziadek: ")
+                print(family[grandfather])
+            else:
+                print("\nBrak wpisu. ")
+        else:
+            print("\nBrak wpisu. ")
+        input("\nWciśnij ENTER aby kontynuować...\n")
+
+    elif choice=='4':
+        son=input("Wpisz Imię Nazwisko dziecka: ")
+        if son in family:
+            print("Ta osoba posiada już wpis, usuń lub zmodyfikuj go. ")
+        else:
+            father=input("Wpisz Imię Nazwisko ojca: ")
+            family[son]=father
+            print("Dodano wpis. ")
+        input("\nWciśnij ENTER aby kontynuować...\n")
+            
+    elif choice=='5':
+        son=input("Wpisz Imię Nazwisko dziecka: ")
+        if son in family:
+            father=input("Wpisz nowe Imię Nazwisko ojca: ")
+            family[son]=father
+            print("Zmodyfikowano wpis. ")
+        else:
+            print("Brak takiej osoby w słowniku. ")
+        input("\nWciśnij ENTER aby kontynuować...\n")
+        
+    elif choice=='6':
+        son=input("Wpisz Imię Nazwisko dziecka: ")
+        if son in family:
+            del family[son]
+            print("Usunięto wpis. ")
+        else:
+            print("Brak takiej osoby w słowniku. ")
+        input("\nWciśnij ENTER aby kontynuować...\n")
+
+        
